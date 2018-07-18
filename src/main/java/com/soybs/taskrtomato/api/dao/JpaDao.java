b3bc3750d732e73e5bb8b3fc2d6a00ee70c4e293
tkhm <no-reply@tkhm.dev>
2018-07-18 21:55:42 +0900

    Fix auto increment id

--- src/main/java/com/soybs/taskrtomato/api/dao/JpaDao.java
+++ src/main/java/com/soybs/taskrtomato/api/dao/JpaDao.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.hibernate.HibernateException;
--- src/main/java/com/soybs/taskrtomato/api/dto/Tomatoes.java
+++ src/main/java/com/soybs/taskrtomato/api/dto/Tomatoes.java
@@ -5,12 +5,16 @@ import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Tomatoes {
 
+    /** tomatoes_id_seqはDB上のseq用のテーブル(serial指定時自動生成) */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
 
     public String summary;
